7fc9790de1204871668cba028349c503c6490118
chuckleym8 <maxgoetzmann@gmail.com>
2023-03-25 23:27:32 +0000

    The grid works now

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,32 @@
-import * as React from 'react';
-import { useState, useEffect } from "react"
-require 
-import Grid from './components/Grid'
+import * as React from "react";
+import { useState, useEffect } from "react";
+import Grid from "./components/Grid";
 
-var data = require('./solutions.json')
+var data = require("./solutions.json");
 
-const allTitles = []
-for(let i = 0;i < 10; i ++){
-  allTitles.push([data.data[i].node.title])
+const allTitles = [];
+for (let i = 0; i < 10; i++) {
+  allTitles.push([data.data[i].node.title]);
 }
 
 function getRandomNumber(): number {
   return Math.floor(Math.random() * 10);
 }
-const anime = data.data[getRandomNumber()]
-console.log(anime.node.title)
-console.log(anime)
+const anime = data.data[getRandomNumber()];
+console.log(anime.node.title);
+console.log(anime);
 
 const names = [anime.node.title, anime.node.alternative_titles.en];
 const score = anime.node.mean;
 const eps = anime.node.num_episodes;
-const studio = anime.node.studios[0].name
-const genres = anime.node.genres
+const studio = anime.node.studios[0].name;
+const genres = anime.node.genres;
 
-console.log(names)
-console.log(score)
-console.log(eps)
-console.log(studio)
-console.log(genres)
+console.log(names);
+console.log(score);
+console.log(eps);
+console.log(studio);
+console.log(genres);
 
 export default function App() {
   const [previousGuesses, setPreviousGuesses] = useState(["Pingu"]);
@@ -35,60 +34,62 @@ export default function App() {
   const [lastGuess, setLastGuess] = useState(guess);
   const [tries, setTries] = useState(0);
 
-  function guessInput(event: { target: { value: React.SetStateAction<string>; }; }){
+  function guessInput(event: {
+    target: { value: React.SetStateAction<string> };
+  }) {
     setGuess(event.target.value);
-  };
+  }
 
-  function onClick(){
+  function onClick() {
     setLastGuess(guess);
     setTries(tries + 1);
-    setPreviousGuesses([...previousGuesses, guess])
-  };
-
-  if(names.includes(lastGuess)){
-    return(
-     <div>
-      <p>You Win!</p>
-      <p>The solution was {names[0]}/{names[1]}</p>
-     </div>
-    )
-  }
-  if(lastGuess === "debug"){
-    return(
-      <div><p>Secret</p></div>
-    )
+    setPreviousGuesses([...previousGuesses, guess]);
   }
 
-  if(tries > 7){
-    return(
+  if (names.includes(lastGuess)) {
+    return (
       <div>
-      <p>Sorry, you lost</p>
-      <p>The solution was {names[0]}/{names[1]}</p>
-    </div>
-    )
+        <p>You Win!</p>
+        <p>
+          The solution was {names[0]}/{names[1]}
+        </p>
+      </div>
+    );
+  }
+  if (lastGuess === "debug") {
+    return (
+      <div>
+        <p>Secret</p>
+      </div>
+    );
+  }
+
+  if (tries > 7) {
+    return (
+      <div>
+        <p>Sorry, you lost</p>
+        <p>
+          The solution was {names[0]}/{names[1]}
+        </p>
+      </div>
+    );
   }
 
   return (
-    <div className = "App">
+    <div className="App">
       <label>Guess your anime: </label>
-      <input type = "text" 
-        id = "guess" 
-        value = {guess} 
-        onChange={guessInput}>
-      </input>
+      <input type="text" id="guess" value={guess} onChange={guessInput}></input>
       <button onClick={onClick}>Enter</button>
       <p>You have used {tries}/7 tries</p>
-      <Grid guesses={[...previousGuesses, guess]} currentGuess={guess} turn={tries} />
-      <figure>Previous Guesses
+      <Grid guesses={previousGuesses} currentGuess={guess} turn={tries} />
+      <figure>
+        Previous Guesses
         <ul>
           {previousGuesses.map((previousGuesses, index) => (
-            <li key = {index}>{previousGuesses}</li>
+            <li key={index}>{previousGuesses}</li>
           ))}
         </ul>
       </figure>
-      
-
     </div>
   );
 }
-
--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,17 +1,17 @@
 // src/components/Row.js
 
-import React from 'react'
+import React from "react";
 
 export default function Row() {
-
   return (
     <div className="row">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div>Your Guess</div>
+      <div>Year Released</div>
+      <div>MAL Score</div>
+      <div># Episodes</div>
+      <div>Studio</div>
+      <div>Genres</div>
+      <div>Common Recommendations</div>
     </div>
-  )
-  
+  );
 }
