daae2299cab2c8cf5582a2ba84e561d618f77794
Jonathan Li <jli7733>
2023-03-25 12:08:23 -0500

    adding stuff

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import * as React from 'react';
 import { useState, useEffect } from "react"
 
 
-
 export default function App() {
+  const [solution, setSolution] = useState("CRANE")
+
   return (
-    <div>
+    <div className = "App">
       <p>Anime Wordle</p>
+      {solution && <div>Solution is {solution}</div>}
       <button>Wow!</button>
     </div>
   );
