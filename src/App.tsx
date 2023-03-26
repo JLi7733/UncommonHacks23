77d796ea97e0fcedfac1476c0c1f6693ad619e43
Jonathan Li <jli7733>
2023-03-25 19:39:37 -0500

    formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import * as React from 'react';
-<<<<<<< HEAD
 import { useState, useEffect } from "react"
 import Grid from './components/Grid'
-=======
-import { useState, useEffect } from "react";
 import AutocompleteForm from './AutoComplete';
 import { TOTAL_ANIME } from './AutoComplete';
->>>>>>> origin/main
 
 var data = require('./solutions.json')
 var allAnimes = []
@@ -44,28 +40,9 @@ export default function App() {
   return (
     <div className = "App">
       <label>Guess your anime: </label>
-<<<<<<< HEAD
-      <input type = "text" 
-        id = "guess" 
-        value = {guess} 
-        onChange={guessInput}>
-      </input>
-      <button onClick={onClick}>Enter</button>
-      <p>You have used {tries}/7 tries</p>
-      <Grid guesses={previousGuesses} currentGuess={guess} turn={tries} />
-      <figure>Previous Guesses
-        <ul>
-          {previousGuesses.map((previousGuesses, index) => (
-            <li key = {index}>{previousGuesses}</li>
-          ))}
-        </ul>
-      </figure>
-      
-
-=======
+      <Grid guesses={null} currentGuess={null} turn={tries} />
       <AutocompleteForm></AutocompleteForm>
       <p>You have used {tries}/7 tries</p>    
->>>>>>> origin/main
     </div>
   );
 }
